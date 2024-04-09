87aa3d29e5054d1e8c26f8b81cf8352f8f9ecdb9
Anton <satsuk.anton2@gmail.com>
2024-04-09 02:33:43 +0200

    Added background animation to the class "container" + appear/disappear animation while scrolling + renamed style_2.css/scss to _main_c.css/_s.scss

--- script/script.js
+++ script/script.js
@@ -8,6 +8,54 @@ function mobileMenu() {
     menu.classList.toggle("inactive");
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    var contentContainers = document.querySelectorAll(".content-img-container");
+    var bgText = document.querySelector(".bg-text-caps-p");
+
+    var offset = 200;
+
+    window.addEventListener("scroll", function() {
+        contentContainers.forEach(function(container, index) {
+            if (isElementInViewport(container, offset)) {
+                if (!container.classList.contains("visible")) {
+                    container.classList.add("visible");
+                }
+            } else {
+                if (container.classList.contains("visible")) {
+                    container.classList.remove("visible");
+                }
+            }
+        });
+
+        if (isElementInViewport(bgText, offset)) {
+            if (!bgText.classList.contains("visible")) {
+                bgText.classList.add("visible");
+            }
+        } else {
+            if (bgText.classList.contains("visible")) {
+                bgText.classList.remove("visible");
+            }
+        }
+    });
+
+    function isElementInViewport(el, offset) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 - offset &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + offset &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Trigger the scroll event to check initially
+    window.dispatchEvent(new Event('scroll'));
+});
+
+
+
+
+
 
 
 
