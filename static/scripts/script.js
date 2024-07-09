86570cdde87708965a4b5b58752f15bb9b836548
Anton <satsuk.anton2@gmail.com>
2024-07-09 13:31:57 +0200

    FEAT: Added an image slider to the 'About Me' section. + box for text

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -46,6 +46,79 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+
+
+
+ 
+
+
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 500000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+function nextSlideImgClick(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const test_form = document.getElementById("test-form");
 console.log('FJDKFHDJKFHSKDJFH')
 // test_form.addEventListener("click", function test_form () {
--- static/watch_sass.sh
+++ static/watch_sass.sh
@@ -16,5 +16,6 @@ sass --watch scss/style_footer_s.scss css/style_footer_c.css &
 sass --watch scss/style_main_s.scss css/style_main_c.css &
 sass --watch scss/hamburger_s.scss css/hamburger_c.css &
 sass --watch scss/style_expirience_s.scss css/style_expirience_c.css &
+sass --watch scss/about_me_s.scss css/about_me_c.css &
 
 wait
